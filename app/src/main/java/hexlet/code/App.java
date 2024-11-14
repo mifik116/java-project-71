c7c4275bae2e12c8d81d20e72f70a02857920f03
mifik116 <mifta.bulat@gmail.com>
2024-11-14 19:18:36 +0300

    3 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,11 +11,20 @@ import java.util.concurrent.Callable;
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"},defaultValue = "stylish", description = "output format [default: stylish]")
+    private String format;
+
     @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
-    private String help = "idk";
+    private String help;
 
     @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
-    private String version = "idk";
+    private String version;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
